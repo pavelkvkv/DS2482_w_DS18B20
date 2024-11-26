c49c3ec4f39e50d52220c176549ab890a65159c2
Pavel <pavelkv.kv@gmail.com>
2024-11-26 15:17:34 +0300

    Huge changes
    Soft i2c lib support added, DS18b20 auto init in 8-bit mode, public defines and prototypes moved to .c,  busy and reset awaiting w/timeout, union-based ID storage, ID-to-num converter, cyclic device search function, struct bitfield-based configs, some optimization

--- wire.c
+++ wire.c
@@ -1,22 +1,146 @@
-// wire.c:		1-wire related functions...
-#include <s_sysinc.h>
+/**** 
+ *  wire.c:		
+ * Библиотека, которая реализует протокол 1-Wire через I2C-адаптер DS2482.
+ * Поддержка опроса и сканирования термодатчиков DS18B20
+ * 
+ * Автор идеи: bayesiandog
+ * Режиссёр: Павел Карягин
+ * В ролях: DS2482-100 (и -800), SW_I2C_Lib
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <inttypes.h>
+
 
 #include "wire.h"
 
+#include "sw_i2c.h"
 
-unsigned char ROM_NO[8]  = {0};
-unsigned char OWID[2][8] = {0};
-unsigned char crc8;
-int LastDiscrepancy;
-int LastFamilyDiscrepancy;
-int LastDeviceFlag;
+#define TAG "i2c_1wire"
+#include "log.h"
 
+// Valid pointer codes for read register selection
+#define	StatReg							(0xF0)
+#define	DataReg							(0xE1)
+#define	ChanReg							(0xD2)
+#define	ConfigReg						(0xC3)
 
-extern DS2480Config DSconfig;
+// DS2480 commands
+#define OWDSConfig                      (0xD2)
+#define OWDSDeviceResetCommand	   		(0xF0)
+#define	OWDSSetReadPtrCommand			(0xE1)
+#define	OWDSChannelSelect				(0xC3)
+#define OWDSResetCommand				(0xB4)
+#define OWDSReadByteCommand				(0x96)
+#define OWDSTriplet						(0x78)
+#define OWDSWriteByteCommand			(0xA5)
+
+// 1-wire commands
+#define OWSearchCmd                     (0xF0)
+#define OWMatchROMCmd                   (0x55)
+#define OWConvertTCmd                   (0x44)
+#define OWReadPad                       (0xBE)
+#define OWWritePad                      (0x4E)
+#define OWCopyPad                       (0x48)
+
+// Status register bit assesment
+#define STATUS_SBR	(1 << 5)
+#define STATUS_TSB	(1 << 6)
+#define STATUS_DIR	(1 << 7)
+#define STATUS_BUSY (1 << 0)
+#define STATUS_RST  (1 << 4)
+#define STATUS_PPD  (1 << 1)
+
+#define OWID_MAX_ELEMENTS 16
+#define RST_TIMEOUT 5
+#define BSY_TIMEOUT 10
+
+static unsigned char ROM_NO[8]  = {0};
+//static unsigned char OWID[OWID_MAX_ELEMENTS][8] = {0};
+
+union
+{
+	u64 ID;
+	u8 ID8[8];
+} OWIDU[OWID_MAX_ELEMENTS] = {0};
+
+static unsigned char crc8 = 0;
+static int LastDiscrepancy = 0;
+static int LastFamilyDiscrepancy = 0;
+static int LastDeviceFlag = 0;
+
+static sw_i2c_t * i2c_bus = NULL;
+static u8 OWDAddress = 0x18 << 1;
+
+static int OWNext();
+static int OWFirst();
+static u8 OWBusyWait(u8 * stat_register);
+static u8 OWResetWait(void);
+static bool OWBusy(void);
+static bool OWReset(void);
+
+/**
+ * Установка дескриптора используемой шины и адреса DS2482
+ */
+void OWSetBus(sw_i2c_t * bus, u8 deviceaddr)
+{
+	logI("");
+	i2c_bus = bus;
+	OWDAddress = deviceaddr;
+}
+
+/**
+ * Копирует все имеющиеся датчики (ID) в предложенный массив
+ * @return количество копируемых датчиков
+ */
+int OWGetIDs(u64 *IDs, size_t len)
+{
+	memset(IDs, 0, sizeof(u64) * len);
+	for(int i = 0; i < len; i++) 
+	{
+		IDs[i] = OWIDU[i].ID;
+	}
+
+	return len;
+}
+
+/**
+ * Инициализирует контроллер 1-Wire, сканирует датчики и прописывает в них настройки
+ * @return количество найденных датчиков
+ */
+int OWInit()
+{
+	logI("");
+	DS2480Config DSconfig_default = 
+	{
+		.APU = 1,
+		.SPU = 0,
+		.WS = 0
+	};
+	if(OWDWriteConfig(&DSconfig_default) == 1)
+	{
+		logE("Failed to write config");
+		return 0;
+	}
+
+	if(OWResetWait() == 1) 
+	{
+		logW("Reset bus shows empty line");
+		return 0;
+	}
+
+	int count = OWDeviceSearch();
+
+	for (int i = 0; i < count; i++)
+	{
+		DS18B20_init(i);
+	}
+
+	return count;
+}
 
 // writeOneWireByte
 //
@@ -24,15 +148,11 @@ extern DS2480Config DSconfig;
 //
 // wrByte - The byte to be written to the 1-Wire.
 // returns - Nothing
-BYTE OWWriteByte(BYTE wrByte)
+u8 OWWriteByte(u8 wrByte)
 {
-	BYTE buff[1];
-
-	while (OWBusy());
-	I2C_Write(OWDAddress, OWDSWriteByteCommand, 0, 1, &wrByte);
-	do {
-		I2C_Read(OWDAddress, OWOperation, 0, 1, buff);
-	} while ((buff[0] & STATUS_BUSY)); // Wait for busy bit to clear, similar to OWBusy()
+	if(OWBusyWait(NULL) == 1) { return 1; }
+	SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSWriteByteCommand, &wrByte, 1);
+	//if(OWBusyWait(NULL) == 1) { return 1; }
 
 	return 1;
 } // OWWriteByte
@@ -44,44 +164,48 @@ BYTE OWWriteByte(BYTE wrByte)
 //
 // returns the byte read
 //
-BYTE OWReadByte(BYTE readRegister)
+u8 OWReadByte(u8 readRegister)
 {
-	BYTE result;
+	u8 result;
 
-	while (OWBusy());
+	if(readRegister != StatReg)
+		if(OWBusyWait(NULL) == 1) { return 1; }
 
-	I2C_Write(OWDAddress, OWDSSetReadPtrCommand, 0, 1, &readRegister); // set pointer to read
-	I2C_Read(OWDAddress, OWOperation, 0, 1, &result);
+	SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSSetReadPtrCommand, &readRegister, 1);
+	SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, &result, 1);
 
-	while (OWBusy());
+	if(readRegister != StatReg)
+		if(OWBusyWait(NULL) == 1) { return 1; }
 
 	return result;
 } // OWReadByte
 
 
-// resetOneWire			
-//
-// Resets the 1-Wire using I2C through the DS2482.
-//
-// returns result
-//
-BYTE OWReset(void)
+/**
+ * Сброс шины 1-Wire
+ * @return 1 если на шине обнаружен пульс слейва
+ */
+static bool OWReset(void)
 {
-	BYTE reset;
-	BYTE status[1];	
+	//logI("");
+	u8 reset;
+	u8 status;	
+	u32 start_time = xTaskGetTickCount(); // Получаем текущее время
 
-	I2C_Read(OWDAddress, OWDSResetCommand, 0, 1, status);	
+	//I2C_Read(OWDAddress, OWDSResetCommand, 0, 1, status);	
+	SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSResetCommand, 0, 0);
 
 	do {
-		I2C_Read(OWDAddress, OWOperation, 0, 1, status);
-	} while (!(status[0] & STATUS_PPD)); // Wait for Presense Pulse to be detected
+		SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, &status, 1);
+	} while (!(status & STATUS_PPD) && (xTaskGetTickCount() - start_time < RST_TIMEOUT)); // Wait for Presense Pulse to be detected
 	do { 
-		I2C_Read(OWDAddress, OWOperation, 0, 1, status);
-	} while ((status[0] & STATUS_BUSY)); // Wait for busy bit to clear, similar to OWBusy()
+		SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, &status, 1);
+	} while ((status & STATUS_BUSY) && (xTaskGetTickCount() - start_time < BSY_TIMEOUT)); // Wait for busy bit to clear, similar to OWBusy()
 
-	reset = (status[0] & STATUS_PPD);	
+	reset = (status & STATUS_PPD);	
+	//logI("Status: %02X", status);
 
-	return reset;
+	return (bool)reset;
 }
 
 // DS2480 device reset			
@@ -90,107 +214,237 @@ BYTE OWReset(void)
 //
 // returns result
 //
-BYTE OWDReset(void)
+u8 OWDReset(void)
 {
-	unsigned char buff[1];
-	BYTE status;	
+	//logI("");
+	//unsigned char buff[1];
+	u8 status;	
 
 	// checking if 1-Wire busy
-	while (OWBusy());
+	//if(OWBusyWait() == 1) { return 1; }
 
-	I2C_Read(OWDAddress, OWDSDeviceResetCommand, 0, 1, buff);
-   	I2C_Read(OWDAddress, OWOperation, 0, 1, &status);
+	SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSDeviceResetCommand, 0, 0);
+	//SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, &status, 1);
 
-	while (OWBusy());
+	if(OWBusyWait(&status) == 1) { return 1; }
 
 	return status & STATUS_RST;
 }
 
-BYTE OWDWriteConfig(DS2480Config *pDSconfig) {
-  	BYTE config;
+u8 OWDWriteConfig(DS2480Config *pDSconfig) 
+{
+	//logI("");
+  	ASSERT(sizeof(DS2480Config) == 1, "sizeof(DS2480Config) == 1");
+
+	pDSconfig->A͟P͟U͟ = !pDSconfig->APU;
+	pDSconfig->S͟P͟U͟ = !pDSconfig->SPU;
+	pDSconfig->W͟S͟  = !pDSconfig->WS;
+	pDSconfig->res_0  = 0;
+	pDSconfig->res_1  = 1;
 
 	// checking if 1-Wire busy
-	while (OWBusy());
-	config = (pDSconfig->WS)<<3 | (pDSconfig->SPU)<<2 | (pDSconfig->APU);
-	config |= ~config<<4;
-
-	I2C_Write(OWDAddress, OWDSConfig, 0, 1, &config);
-
-	while (OWBusy());
+	if(OWBusyWait(NULL) == 1) { return 1; }
+	SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSConfig, (u8*)pDSconfig, 1);
+	DS2480Config tmp = {0};
+	SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, (u8*)&tmp, 1);
+	if(tmp.APU == pDSconfig->APU && tmp.SPU == pDSconfig->SPU && tmp.WS == pDSconfig->WS && tmp.res_0 == 0) return 0;
 	return 1;
 }
 
 
-																	 
-// returns 0 if 1wire is busy, needs timer so it doesn't stay here forever
-BYTE OWBusy(void)
+
+
+/**
+ * @brief Проверяет занятость 1-Wire интерфейса.
+ *
+ * @return 1 если устройство занято, 0 если нет.
+ */
+static bool OWBusy(void)
 {
-	BYTE status = StatReg;
-	BYTE buff;
+	u8 buff;
+	u8 reg = StatReg;
+	SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSSetReadPtrCommand, &reg, 1);
+	SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, &buff, 1);
 
-	I2C_Write(OWDAddress, OWDSSetReadPtrCommand, 0, 1, &status);		// set pointer to read	
-	I2C_Read(OWDAddress, OWOperation, 0, 1, &buff);						// read
-
-	return buff & (1<<0);
+	return (bool)(buff & (STATUS_BUSY));
 }
 
-WORD DS18B20_readTemp(BYTE deviceNumber)
+/**
+ * @brief Проверяет занятость 1-Wire интерфейса с таймаутом.
+ *
+ * Функция отправляет команду для проверки статуса устройства, и если устройство занято,
+ * ожидает его освобождения с ограничением по времени.
+ * @param stat_register - указатель на байт, в который будет записан статус устройства
+ *
+ * @return 0 если устройство готово, 1 если истек таймаут.
+ */
+static u8 OWBusyWait(u8 * stat_register)
 {
-    char hak[200];
-    char ret[9];
-    WORD temp;
+    u32 start_time = xTaskGetTickCount(); // Получаем текущее время
 
-    while(!OWReset()); 
-    OWWriteByte(OWMatchROMCmd); // Match ROM
-   
-    for (int i=7;i>=0;i--) {
-        OWWriteByte(OWID[deviceNumber][i]);
+	u8 * stat_register_buff = NULL;
+	u8 stat_register_tmp = 0;
+	if(stat_register != NULL) stat_register_buff = stat_register;
+	else stat_register_buff = &stat_register_tmp;
+
+	bool i2c_ret = true;
+	u8 StatReg_addr = StatReg;
+	i2c_ret = SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSSetReadPtrCommand, &StatReg_addr, 1);
+	if(i2c_ret == false) return 1;
+
+    // Ожидаем, пока устройство занято, или пока не истечет таймаут
+    while (1)
+    {
+		i2c_ret = SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, stat_register_buff, 1);
+        // Если устройство не занято, выходим
+        if ((*stat_register_buff & (STATUS_BUSY)) == 0 && i2c_ret == true)
+        {
+			//logI("Busy OK");
+            return 0; // Устройство готово
+        }
+        // Проверяем таймаут
+        if ((xTaskGetTickCount() - start_time) > BSY_TIMEOUT)
+        {
+			logW("Busy Timeout");
+            return 1; // Таймаут истек
+        }
     }
-    DSconfig.APU = 1;
-    DSconfig.SPU = 1;
-    DSconfig.WS = 0;
-    OWDWriteConfig(&DSconfig);
+}
 
-    OWWriteByte(OWConvertTCmd); // Convert T
+static u8 OWResetWait(void)
+{
+    u32 start_time = xTaskGetTickCount(); // Получаем текущее время
 
-    Delay(1000); // Wait for measurement
-    while(!OWReset());
+    // Ожидаем, пока устройство занято, или пока не истечет таймаут
+    while (1)
+    {
+        // 
+        if (OWReset() == 1)
+        {
+			//logI("Reset Bus OK");
+            return 0; // Устройство готово
+        }
+        // Проверяем таймаут
+        if ((xTaskGetTickCount() - start_time) > RST_TIMEOUT)
+        {
+			logW("Reset Bus Timeout");
+            return 1; // Таймаут истек
+        }
+    }
+}
+
+
+int DS18B20_init(int deviceNumber)
+{	
+	if(deviceNumber >= OWID_MAX_ELEMENTS || OWIDU[deviceNumber].ID < 0) return 0;
+
+	if(OWResetWait() == 1) return 1;
+    OWWriteByte(OWMatchROMCmd); // Match ROM
+   
+    for (int i=7;i>=0;i--) 
+	{
+        OWWriteByte(OWIDU[deviceNumber].ID8[i]);
+    }
+	OWWriteByte(OWWritePad); // Write Scratchpad
+	DS18B20Config Tconfig = 
+	{
+		.config.config.ones = 0b11111,
+		.config.config.zero = 0,
+		.config.config.r0 = 0,
+		.config.config.r1 = 0,
+		.user_0 = 0xFF, 
+		.user_1 = 0xFF
+	};
+	OWWriteByte(Tconfig.user_0);
+	OWWriteByte(Tconfig.user_1);
+	OWWriteByte(Tconfig.config.config_byte);
+
+	portENTER_CRITICAL();
+	OWWriteByte(OWCopyPad); // Copy Scratchpad
+	DS2480Config DSconfig_spu = 
+	{
+		.APU = 1,
+		.SPU = 1,
+		.WS = 0
+	};
+    OWDWriteConfig(&DSconfig_spu); // enable strong pull up and std speed
+	portEXIT_CRITICAL();
+
+	vTaskDelay(10);
+
+	return 0;	
+}
+
+u16 DS18B20_readTemp(int deviceNumber)
+{
+	if(deviceNumber >= OWID_MAX_ELEMENTS || OWIDU[deviceNumber].ID < 0) return 0;
+
+    if(OWResetWait() == 1) return 0;
     OWWriteByte(OWMatchROMCmd); // Match ROM
    
     for (int i=7;i>=0;i--) {
-        OWWriteByte(OWID[deviceNumber][i]);
+        OWWriteByte(OWIDU[deviceNumber].ID8[i]);
+    }
+
+	DS2480Config DSconfig = 
+	{
+		.APU = 1,
+		.SPU = 1,
+		.WS = 0
+	};
+    
+	portENTER_CRITICAL();
+    OWWriteByte(OWConvertTCmd); // Convert T
+	OWDWriteConfig(&DSconfig); // SPU enable
+	portEXIT_CRITICAL();
+
+    vTaskDelay(100); // Wait for measurement
+    if(OWResetWait() == 1) return 0;
+    OWWriteByte(OWMatchROMCmd); // Match ROM
+   
+    for (int i=7;i>=0;i--) {
+        OWWriteByte(OWIDU[deviceNumber].ID8[i]);
     }
 
 
     OWWriteByte(OWReadPad); // Read Scratchpad
-    hak[0] = DataReg;
-    for (int i=0;i<9;i++) {
-        I2C_Write(OWDAddress, OWDSReadByteCommand, 0, 0, hak);
-        Delay(10);	  	
-        I2C_Write(OWDAddress, OWDSSetReadPtrCommand, 0, 1, hak);		// set pointer to read
-        Delay(10);
-        I2C_Read(OWDAddress, OWOperation, 0, 1, &ret[i]);
+	OWBusyWait(NULL);
+    u8 ptr = DataReg;
+	u8 ret[2];
+    for (int i=0;i<2;i++) {
+        SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSReadByteCommand, 0, 0);
+        vTaskDelay(2);	  	
+        SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSSetReadPtrCommand, &ptr, 1);		// set pointer to read
+        //vTaskDelay(10);
+        SW_I2C_Read_Noaddr(i2c_bus, OWDAddress, &ret[i], 1);
     }
-    temp = (ret[1]<<8 | ret[0]);
-    sprintf(hak, "celcius = %.04f\n", temp*0.0625);
-	CommSend(COMM_EXT, hak);
+    u16 temp = (ret[1]<<8 | ret[0]);
+	//logI("Got temperature: %.01f", temp*0.0625);
 
-    return 1;
+    return (int)(temp*0.0625);
 
 }
 
 
 //--------------------------------------------------------------------------
 // Perform a search for all devices on the 1-Wire network  	  
-//
-int OWDeviceSearch() 
+int OWDeviceSearch()
 {
-    BYTE result;
+	u8 result = 0;
 
-    if (OWFirst()==1)
-        while(OWNext()==1);
+	if (OWFirst() != 0)
+	{
+		while (OWNext() != 0);
+
+		// scan array
+		for (result = 0; result < OWID_MAX_ELEMENTS; result++)
+		{
+			if (OWIDU[result].ID == 0) break;
+		}
+	}
+	logI("Found %d devices", result);
+	return result;
 }
-
 
 //--------------------------------------------------------------------------
 // Find the 'first' devices on the 1-Wire network
@@ -243,7 +497,7 @@ int OWSearch()
 	int last_zero, rom_byte_number, search_result;
 	int id_bit, cmp_id_bit;
 	unsigned char rom_byte_mask, search_direction, status;
-  	static BYTE iter = 0;
+  	static u8 iter = 0;
 	// initialize for search
 	id_bit_number = 1;
 	last_zero = 0;
@@ -260,7 +514,7 @@ int OWSearch()
                 LastDiscrepancy = 0;
                 LastDeviceFlag = 0;
                 LastFamilyDiscrepancy = 0;
-                return false;
+                return 0;
             }
             
 		// issue the search command
@@ -352,20 +606,20 @@ int OWSearch()
         int i,j;
     if (search_result) {
         for (i=0, j=7;i<8;i++, j--) {
-            OWID[iter][j] = ROM_NO[i]; // save ID ROM in 2D array OWID
+            OWIDU[iter].ID8[j] = ROM_NO[i]; // save ID ROM in 2D array OWID
         }     
-        sprintf(buf, "ID = %02X%02X%02X%02X%02X%02X%02X%02X\n", OWID[iter][0], OWID[iter][1], OWID[iter][2], OWID[iter][3], OWID[iter][4], OWID[iter][5], OWID[iter][6], OWID[iter][7]);
-        CommSend(COMM_EXT, buf);
+        logI("ID = %"PRIX64, OWIDU[iter].ID);
+        
 
         ++iter;
         if (LastDeviceFlag) {
-            CommSend(COMM_EXT, "LAST DEVICE\n");   
+            logI("LAST DEVICE");   
             iter = 0;
             return 2;
         }
     }
     else {
-        CommSend(COMM_EXT, "NO DEVICES FOUND\n");
+        logI("NO DEVICES FOUND");
         iter = 0;
     }
 	return search_result;
@@ -394,11 +648,22 @@ unsigned char DS2482_search_triplet(int search_direction)
 	// SS indicates byte containing search direction bit value in msbit
 
 	// checking if 1-Wire busy
-	while (OWBusy());
+	if(OWBusyWait(NULL) == 1) { return 1; }
     hak[0] = search_direction ? 0x80 : 0x00;
-	I2C_Write(OWDAddress, OWDSTriplet, 0, 1, hak);
-	while (OWBusy());
-	status = OWReadByte(StatReg);
+	//I2C_Write(OWDAddress, OWDSTriplet, 0, 1, hak);
+	SW_I2C_Write_8addr(i2c_bus, OWDAddress, OWDSTriplet, 0, 0);
+	if(OWBusyWait(&status) == 1) { return 1; }
+	//status = OWReadByte(StatReg);
 	// return status byte
 	return status;
+}
+
+int OWAddr2Num(u64 OWID)
+{
+	for(int i = 0; i < OWID_MAX_ELEMENTS; i++)
+	{
+		if(OWIDU[i].ID == OWID)
+			return i;
+	}
+	return -1;
 }
--- wire.h
+++ wire.h
@@ -2,61 +2,62 @@
 #ifndef __WIRE_H
 #define __WIRE_H
 
-#define OWDAddress						(0x30)
+#include "my_types.h"
+#include "sw_i2c.h"
 
-// Valid pointer codes for read register selection
-#define	StatReg							(0xF0)
-#define	DataReg							(0xE1)
-#define	ChanReg							(0xD2)
-#define	ConfigReg						(0xC3)
 
-// DS2480 commands
-#define OWDSConfig                      (0xD2)
-#define OWDSDeviceResetCommand	   		(0xF0)
-#define	OWDSSetReadPtrCommand			(0xE1)
-#define	OWDSChannelSelect				(0xC3)
-#define OWDSResetCommand				(0xB4)
-#define OWDSReadByteCommand				(0x96)
-#define OWDSTriplet						(0x78)
-#define OWDSWriteByteCommand			(0xA5)
 
-// 1-wire commands
-#define OWSearchCmd                     (0xF0)
-#define OWMatchROMCmd                   (0x55)
-#define OWConvertTCmd                   (0x44)
-#define OWReadPad                       (0xBE)
 
-// Status register bit assesment
-#define STATUS_SBR	(0x20)
-#define STATUS_TSB	(0x40)
-#define STATUS_DIR	(0x80)
-#define STATUS_BUSY (0x01)
-#define STATUS_RST  (0x10)
-#define STATUS_PPD  (0x02)
-
-typedef uint8_t BYTE;
-
-// I2C value for reading only
-#define OWOperation (0x69)
 
 // DS2480 config structure
-typedef struct {
-    BYTE APU; // Active pull up
-    BYTE SPU; // Strong pull up
-    BYTE WS;  // Wire speed
+typedef struct __attribute__((packed, aligned(1))) 
+{
+    bool APU    : 1; // Active pull up
+    bool res_0  : 1; // always 0
+    bool SPU    : 1; // Strong pull up
+    bool WS     : 1; // Wire speed (0 - std, 1 - fast)
+    bool A͟P͟U͟    : 1; // Active pull up (inverted)
+    bool res_1  : 1; // always 1
+    bool S͟P͟U͟    : 1; // Strong pull up (inverted)
+    bool W͟S͟     : 1; // Wire speed (inverted)
 } DS2480Config;
+
+// DS18B20 config structure
+typedef struct __attribute__((packed, aligned(1))) 
+{
+    u8 user_0   ;    // user byte
+    u8 user_1   ;    // user byte
+    union 
+    {
+    u8 config_byte;    
+    struct              // config byte  00 - 100 ms, 01 - 200 ms, 10 - 400 ms, 11 - 800 ms
+    {
+    u8 ones     : 5;    // always 1
+    bool r0     : 1;    // cfg resolution (delay)
+    bool r1     : 1;    // cfg resolution (delay)
+    bool zero   : 1;    // always 0
+    }config;
+    }config;
+} DS18B20Config;
+
 
 
 
 // Functions
-BYTE OWWriteByte(BYTE wrByte);
-BYTE OWReadByte(BYTE rdByte);
-BYTE OWReset(void);
-BYTE OWDReset(void);
-BYTE OWDWriteConfig(DS2480Config *pDSconfig);
-BYTE OWBusy(void);
-WORD DS18B20_readTemp(BYTE deviceNumber);
+int OWInit();
+void OWSetBus(sw_i2c_t * bus, u8 deviceaddr);
+int OWGetIDs(u64 *IDs, size_t len);
+u8 OWWriteByte(u8 wrByte);
+u8 OWReadByte(u8 rdByte);
+
+u8 OWDReset(void);
+u8 OWDWriteConfig(DS2480Config *pDSconfig);
+
+u16 DS18B20_readTemp(int deviceNumber);
+int DS18B20_init(int deviceNumber);
+int OWDeviceSearch();
 int OWSearch(void);
 unsigned char DS2482_search_triplet(int search_direction);
+int OWAddr2Num(u64 OWID);
 
 #endif
